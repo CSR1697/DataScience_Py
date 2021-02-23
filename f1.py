e58bbc3d176789659570d15e6bf3970d4d6b3883
Chandru01061997 <csr.kolli55@gmail.com>
2021-02-23 09:16:00 +0530

    New code from List assignment to cloud

--- f1.py
+++ f1.py
@@ -1,2 +1,10 @@
-This is my new Commit
-i am new to python
+my_lst = eval(input("Enter the list: "))
+ind = 0
+out_mul = 1
+print(type(my_lst))
+print("Length of the list is: ", len(my_lst))
+for i in my_lst:
+    out_mul *= my_lst[ind]
+    ind += 1
+print("The multiplication of all elements in  the list is: ",out_mul)
+
